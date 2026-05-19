87f5b2fdd76143320fe818b4bc7552e28504d06f
Mutasa Elizabeth <lisamutasa435@gmail.com>
2026-05-19 10:19:01 +0300

    Implement Collatz sequence and distinct number count

--- exercises/basics.py
+++ exercises/basics.py
@@ -12,7 +12,14 @@ def collatz(n: int) -> List[int]:
     For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
     So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
     """
-    pass
+    result = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        result.append(n)
+    return result
 
 
 def distinct_numbers(numbers: List[int]) -> int:
@@ -21,4 +28,4 @@ def distinct_numbers(numbers: List[int]) -> int:
 
     E.g if numbers = [2, 3, 2, 2, 3], then the answer is 2 since there are only 2 unique numbers: 2 and 3.
     """
-    pass
+    return len(set(numbers))
